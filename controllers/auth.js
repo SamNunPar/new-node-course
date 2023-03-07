8a330cf7fbe86752938ab51f69c4edb0f8833551
SamNunPar <125912081+SamNunPar@users.noreply.github.com>
2023-03-07 15:44:15 -0600

    Login con Google completo
    Se termino el login con google ademas del logout, aparte se hizo update del modelo del usuario para que el rol tuviera un valor por default

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const { response } = require('express')
 const Usuario = require('../models/usuario')
 const bcryptjs = require('bcryptjs')
 const { generarJWT } = require('../helpers/generar-jwt')
+const { googleVerify } = require('../helpers/google-verify')
 
 
 const login = async(req, res = response) => {
@@ -9,7 +10,6 @@ const login = async(req, res = response) => {
     const { correo, password } = req.body
 
     try{
-
         // Verifica existencia de email
         const usuario = await Usuario.findOne({ correo })
         if ( !usuario ) {
@@ -27,7 +27,7 @@ const login = async(req, res = response) => {
         }
 
 
-        // Verificae contraseña
+        // Verifica contraseña
         const validPassword = bcryptjs.compareSync( password, usuario.password )
         if ( !validPassword ) {
             return res.status(400).json({
@@ -58,13 +58,48 @@ const googleSignIn = async( req, res = response ) => {
 
     const { id_token } = req.body;
 
-    
+    try {
+        const { nombre, img, correo } = await googleVerify( id_token );
+
+        let usuario = await Usuario.findOne({ correo })
+
+        if ( !usuario ) {
+            // Creacion del usuario
+            const data = {
+                nombre,
+                correo,
+                password: ':P',
+                img,
+                google: true,
+                rol: 'USER_ROL'
+            };
+
+            usuario = new Usuario( data );
+            await usuario.save();
+        }
+
+        if ( !usuario.estado ) {
+            return res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            })
+        }
+
+        // Generar JWT (JSON Web Token)
+        const token= await generarJWT( usuario.id )
 
 
-    res.json({
-        msg: 'Todo ok',
-        id_token
-    })
+        res.json({
+            usuario,
+            token
+        })
+    } catch (error) {
+        res.status(400).json({
+            ok: false,
+            msg: 'El token no se pudo verificar'
+        })
+    }
+
+
 }
 
 
